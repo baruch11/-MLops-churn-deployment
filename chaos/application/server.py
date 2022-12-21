b9108ee7315a75f9ee257ab8a66fcef6110511ae
CHARLES-HENRI PRAT <charprat@yahoo.fr>
2022-12-21 17:41:45 +0100

    API documentation in server + types changes in request

--- chaos/application/server.py
+++ chaos/application/server.py
@@ -1,36 +1,78 @@
+"""API definition for churn detection."""
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 from chaos.domain.customer import Customer
 import pandas as pd
-
-app = FastAPI()
-
-DEFAULT_RESPONSE = -1
+from typing import Optional, Literal
+from datetime import datetime
 
 
 class Question(BaseModel):
-    BALANCE: float = 0.0
-    NB_PRODUITS: int = 0
-    CARTE_CREDIT: bool = True
-    SALAIRE: float = 0.0
-    SCORE_CREDIT: float = 0.0
-    DATE_ENTREE: str = "2010"
-    NOM: str = ""
-    PAYS: str = 'France'
-    SEXE: bool = False
-    AGE: int = 40
-    MEMBRE_ACTIF: bool = False
+    """Churn detection parameters.
+
+    Parameters
+    ----------
+    BALANCE : float
+        customer bank account balance
+    NB_PRODUITS : int
+        how many products the customer owns
+    CARTE_CREDIT : bool
+        true if the customer has a credit card
+    SALAIRE : float
+        customer's salary
+    SCORE_CREDIT : float
+        customer score for credit allocation
+    DATE_ENTREE : datetime
+        optional (fot futur use), date of the customer subscription
+        date in format "YYYY-MM-DD HH:MM"
+    PAYS : str
+        country of the customer
+    SEXE : str
+        customer's gender, 'H' (Male) of 'F' (Female)
+    AGE : int
+        customer's age
+    MEMBRE_ACTIF:
+        true if the customer is active on his bank account
+    """
+
+    BALANCE: float = None
+    NB_PRODUITS: int = None
+    CARTE_CREDIT: bool = None
+    SALAIRE: float = None
+    SCORE_CREDIT: float = None
+    DATE_ENTREE: Optional[datetime] = None
+    PAYS: str = None
+    SEXE: Literal["H", "F"] = None
+    AGE: int = None
+    MEMBRE_ACTIF: bool = None
+
+
+app = FastAPI(
+    title="Churn detection",
+    openapi_tags=[{
+        "name": "detect",
+        "description": ("Give a probability of churn "
+                        "given customer's characteristics")
+    }]
+)
 
 
 class Answer(BaseModel):
+    """Churn detection response."""
+
     answer: float
 
 
-@app.post("/example/")
-def example(q: Question):
-    try:
-        customer = Customer(pd.DataFrame([q.dict()]))
-        answer = customer.predict_subscription()
-    except (ValueError, TypeError, KeyError):
-        answer = DEFAULT_RESPONSE
+@app.post("/detect/", tags=["detect"])
+def detect(q: Question):
+    """Call Customer model churn detection.
+
+    Parameters
+    ----------
+    q : Question(BaseModel)
+        Customer marketing characterics
+    """
+    customer = Customer(pd.DataFrame([q.dict()]))
+    answer = customer.predict_subscription()
+
     return Answer(answer=answer)
