91b29cfd133877d9021dbb82e6c44b5c048df1c5
“Emeric <emeric.colombet@2020.icam.fr>
2023-01-17 11:46:22 +0100

    Testing historicize root in functional tests, adding some data

--- chaos/test/functional/test_bdd.py
+++ chaos/test/functional/test_bdd.py
@@ -19,7 +19,20 @@ expected_data = {"ID_CLIENT": [15688172],
                  "SCORE_CREDIT": [677.0],
                  "CHURN": ["No"]
                  }
-
+customer_input = {"ID_CLIENT": 15688172,
+                  "DATE_ENTREE": "2015-06-01",
+                  "NOM": "Tai",
+                  "PAYS": "Espagne",
+                  "SEXE": "H",
+                  "AGE": 40,
+                  "MEMBRE_ACTIF": "No",
+                  "BALANCE": 0.0,
+                  "NB_PRODUITS": 2,
+                  "CARTE_CREDIT": "Yes",
+                  "SALAIRE": 88947.56,
+                  "SCORE_CREDIT": 677.0,
+                  "CHURN": "No"
+                  }
 
 class TestCustomerLoader:
 
@@ -40,11 +53,15 @@ class TestCustomerLoader:
         line_15688172 = customer_loader.find_a_customer(15688172)
         expected_df = pd.DataFrame(data=expected_data)
         assert expected_df.equals(line_15688172)
-    
+ 
     def test_predict_from_id(self):
         with TestClient(app) as client: 
             response = client.get("/customer_detect/15791700")
             assert response.status_code == 200 
             assert response.json()["answer"] == 0.9769012533043302
 
-        
+    def test_historicize_data_and_retrieve_it(self):
+        customer_loader = CustomerLoader()
+        customer_loader.historicize_api_calls(
+            customer_input=customer_input,
+            prediction=0.22)
